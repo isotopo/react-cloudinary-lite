cdf62e822416150a1dd9f1154c0728c2f6e5661c
diego-d5000 <diego-d@outlook.com>
2016-07-06 18:43:18 -0500

    [ADD] tests in jest for src props

---
+++ __tests__/CloudinaryLite-test.js
@@ -0,0 +1,34 @@
+jest.unmock('../src/index.js');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import CloudinaryLite from '../src/index.js';
+
+describe('CloudinaryLite', () => {
+
+  it('render cloudinary demo image with basic url given a src prop  ', () => {
+    // Render a checkbox with label in the document
+    const demoImg = TestUtils.renderIntoDocument(
+        <CloudinaryLite cloudName="demo" src="sample.jpg" />
+    );
+
+    const demoImgNode = ReactDOM.findDOMNode(demoImg);
+
+    // verify img src
+    expect(demoImgNode.getAttribute('src')).toEqual('https://res.cloudinary.com/demo/image/upload/sample.jpg');
+  });
+
+  it('render cloudinary demo image with resize and crop url  ', () => {
+    // Render a checkbox with label in the document
+    const demoImg = TestUtils.renderIntoDocument(
+        <CloudinaryLite cloudName="demo" src="sample.jpg" transformations={{width:300, height: 200, crop: 'crop'}} />
+    );
+
+    const demoImgNode = ReactDOM.findDOMNode(demoImg);
+
+    // verify img src
+    expect(demoImgNode.getAttribute('src')).toEqual('https://res.cloudinary.com/demo/image/upload/w_300,h_200,c_crop/sample.jpg');
+  });
+
+});
